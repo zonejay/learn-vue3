8a88dbc4603ecc912cc14aa1751b8b4f267fd55b
zyj356a <zyj1179035793@gmail.com>
2022-08-09 09:47:18 +0800

    basic effect function

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+let bucket = new Set()
+const obj = { text: 'hello world' }
+const data = new Proxy(obj, {
+    get(target, key) {
+        bucket.add(effect)
+        return target[key]
+    },
+    set(target, key, value) {
+        target[key] = value
+        bucket.forEach(fn => fn())
+        return true
+    }
+})
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function effect() {
+    document.querySelector("#app").innerHTML = data.text
+}
 
-setupCounter(document.querySelector('#counter'))
+effect()
+
+setTimeout(() => {
+    data.text = "hello vue"
+}, 1000);

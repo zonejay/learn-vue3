bde5c9cf2c27e087a16c49f1689e91768007c611
zyj356a <zyj1179035793@gmail.com>
2022-08-10 16:36:31 +0800

    avoid effect function run repeatly in condition switch

--- main.js
+++ main.js
@@ -4,7 +4,14 @@
 // 这个问题
 let bucket = new WeakMap()
 let activeEffect
-const obj = { text: 'hello world' }
+const obj = { text: 'hello world', show:true }
+function cleanup(effectFn) {
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        const deps = effectFn.deps[i];
+        deps.delete(effectFn)
+    }
+    effectFn.deps.length = 0
+}
 function track(target,key) {
     let depsMap
     if(bucket.has(target)) {
@@ -19,13 +26,17 @@ function track(target,key) {
         depsMap.set(key, (deps = new Set()))
     }
     deps.add(activeEffect)
+    activeEffect.deps.push(deps)
 }
 function trigger(target,key) {
     if(bucket.has(target)) {
         let depsMap = bucket.get(target)
         if(depsMap.has(key)) {
             let deps = depsMap.get(key)
-            deps.forEach(fn => fn())
+            // set的forEach如果同一个元素在删除的同时被加进set，不会被标记为已被访问
+            // 会造成死循环，可以使用一个临时的set
+            const cacheDeps = new Set(deps)
+            cacheDeps.forEach(fn => fn())
         }
     }
 }
@@ -42,17 +53,25 @@ const data = new Proxy(obj, {
 })
 
 const effect = (fn) => {
-    activeEffect = fn
-    fn()
+    const effectFn = () => {
+        cleanup(effectFn)
+        activeEffect = effectFn
+        fn()
+    }
+    // deps数组缓存bucket中的set
+    effectFn.deps = []
+    effectFn()
 }
 
 effect(() => {
+    // 在副作用函数中，初始show为true，当show为false的时候再去改变text的值
+    // 会重新触发副作用函数。可以执行清理操作避免分支切换的时候执行多余的副作用
+    // 函数，因为每次执行副作用函数都会重新出发track，可以在track函数中清除依赖
     console.log('execed');
-    document.querySelector("#app").innerHTML = data.text
+    document.querySelector("#app").innerHTML = data.show ? data.text:'none'
 })
 
 setTimeout(() => {
+    data.show = false
     data.text = "hello vue"
-    data.year = 2019
-    data.day = 9
 }, 1000);

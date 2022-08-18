8773d14555071d0e5e1b3170bfd64de1364be4e8
moon <zyj1179035793@gmail.com>
2022-08-18 22:29:09 +0800

    lint file

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ let bucket = new WeakMap()
 let activeEffect
 // 副作用函数栈，栈顶存放当前的副作用函数
 const effectStack = []
-const obj = { text: 'hello world', show:true, count:0 }
+const obj = { text: 'hello world', show: true, count: 0, firstName: 'john', lastName: 'smith' }
 const jobQueue = new Set()
 const p = Promise.resolve()
 
 // 刷新标识
 let isFlushing = false
 function flushJob() {
-    if(isFlushing) return
+    if (isFlushing) return
     // 标识设为true 表示正在刷新
     isFlushing = true
     p.then(() => {
@@ -30,16 +30,16 @@ function cleanup(effectFn) {
     }
     effectFn.deps.length = 0
 }
-function track(target,key) {
-    if(!activeEffect) return
+function track(target, key) {
+    if (!activeEffect) return
     let depsMap
-    if(bucket.has(target)) {
+    if (bucket.has(target)) {
         depsMap = bucket.get(target)
     } else {
-        bucket.set(target,(depsMap = new Map()))
+        bucket.set(target, (depsMap = new Map()))
     }
     let deps
-    if(depsMap.has(key)) {
+    if (depsMap.has(key)) {
         deps = depsMap.get(key)
     } else {
         depsMap.set(key, (deps = new Set()))
@@ -48,22 +48,22 @@ function track(target,key) {
     // console.log(activeEffect);
     activeEffect.deps.push(deps)
 }
-function trigger(target,key) {
-    if(bucket.has(target)) {
+function trigger(target, key) {
+    if (bucket.has(target)) {
         let depsMap = bucket.get(target)
-        if(depsMap.has(key)) {
+        if (depsMap.has(key)) {
+            console.log('trigger');
             let deps = depsMap.get(key)
             // set的forEach如果同一个元素在删除的同时被加进set，不会被标记为已被访问
             // 会造成死循环，可以使用一个临时的set
             const effectToRun = new Set()
             deps && deps.forEach(fn => {
-                if(fn !== activeEffect) {
+                if (fn !== activeEffect) {
                     effectToRun.add(fn)
                 }
             })
             effectToRun.forEach(fn => {
-                if(fn.config.schedule) {
-                    // setTimeout(fn)
+                if (fn.config.schedule) {
                     fn.config.schedule(fn)
                 } else {
                     fn()
@@ -75,12 +75,12 @@ function trigger(target,key) {
 const data = new Proxy(obj, {
     get(target, key) {
         // console.log('track');
-        track(target,key)
+        track(target, key)
         return target[key]
     },
     set(target, key, value) {
         target[key] = value
-        trigger(target,key)
+        trigger(target, key)
         return true
     }
 })
@@ -91,53 +91,84 @@ const effect = (fn, config = {}) => {
         activeEffect = effectFn
         // fn是真正的副作用函数，在执行之前压入栈顶
         effectStack.push(effectFn)
-        fn()
+        const res = fn()
         // 执行完成之后推出当前副作用
         effectStack.pop()
 
         // activeEffect始终指向栈顶
         activeEffect = effectStack[effectStack.length - 1]
+        return res
     }
     // deps数组缓存bucket中的set
     effectFn.deps = []
     console.log(config);
     effectFn.config = config
-    effectFn()
+    if (!config.lazy) {
+        effectFn()
+    }
+    return effectFn
 }
 
-effect(() => {
-    // 在副作用函数中，初始show为true，当show为false的时候再去改变text的值
-    // 会重新触发副作用函数。可以执行清理操作避免分支切换的时候执行多余的副作用
-    // 函数，因为每次执行副作用函数都会重新出发track，可以在track函数中清除依赖
-    // document.querySelector("#app").innerHTML = data.count
-    // effect(() => {
-    //     console.log('nesting effect');
-    //    data.show = false
-    // })
-    // document.querySelector("#app").innerHTML = data.text
-    // console.log('execed');
-    // 自增操作同样会引起无限递归循环，解决方法是在trigger函数中判断要执行的副作用
-    // 函数是不是acitveEffect
-    // data.count++
-    console.log(data.count);
-},{
-    // 调度器，决定何时运行副作用函数
-    // schedule(fn){
-    //     setTimeout(fn)
-    // },
-    schedule(fn) {
-        jobQueue.add(fn)
-        flushJob()
+// effect(() => {
+//     // 在副作用函数中，初始show为true，当show为false的时候再去改变text的值
+//     // 会重新触发副作用函数。可以执行清理操作避免分支切换的时候执行多余的副作用
+//     // 函数，因为每次执行副作用函数都会重新出发track，可以在track函数中清除依赖
+//     // document.querySelector("#app").innerHTML = data.count
+//     // effect(() => {
+//     //     console.log('nesting effect');
+//     //    data.show = false
+//     // })
+//     // document.querySelector("#app").innerHTML = fullname.value
+//     // console.log('execed');
+//     // 自增操作同样会引起无限递归循环，解决方法是在trigger函数中判断要执行的副作用
+//     // 函数是不是acitveEffect
+//     // data.count++
+//     console.log(data.count);
+// }, {
+//     // 调度器，决定何时运行副作用函数
+//     // schedule(fn){
+//     //     setTimeout(fn)
+//     // },
+//     // schedule(fn) {
+//     //     jobQueue.add(fn)
+//     //     flushJob()
+//     // }
+//     lazy: false
+// })
+function computed(getter) {
+    // 缓存值
+    let value
+    let dirty = true
+    const effectFn = effect(getter, {
+        lazy: true, schedule() {
+            console.log('change dirty');
+            dirty = true
+        }
+    })
+    const obj = {
+        get value() {
+            if (dirty) {
+                value = effectFn()
+                dirty = false
+            }
+            return value
+        }
     }
-    // lazy:true
+    return obj
+}
+data.count++
+data.count++
+
+const fullname = computed(() => {
+    console.log('computed effect');
+    return data.firstName + ' ' + data.lastName
 })
-data.count++
-data.count++
-console.log('test');
+console.log(fullname.value);
+// console.log(fullname.value);
+// data.firstName = 'sb'
+// effect(() => {
+//     document.querySelector("#app").innerHTML = fullname.value
+// })
 // setTimeout(() => {
-//     // data.show = false
-//     // data.text = "hello vue"
-//     data.count++
-//     // console.log(data.show);
-//     // console.log(data.count);
+//     data.firstName = 'haruhi'
 // }, 1000);
